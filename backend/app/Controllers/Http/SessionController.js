ff2ed847d3bd39111b1f5e207579fee24f391adc
Fernando de Bona <fbonareis@gmail.com>
2019-10-17 00:31:04 -0300

    create session feature

---
+++ backend/app/Controllers/Http/SessionController.js
@@ -0,0 +1,13 @@
+'use strict';
+
+class SessionController {
+  async store({ request, auth }) {
+    const { email, password } = request.only(['email', 'password']);
+
+    const token = await auth.attempt(email, password);
+
+    return { token };
+  }
+}
+
+module.exports = SessionController;
---
+++ backend/test/functional/session.spec.js
@@ -0,0 +1,27 @@
+const { test, trait } = use('Test/Suite')('Session');
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use('Factory');
+
+trait('Test/ApiClient');
+trait('DatabaseTransactions');
+
+test('it should return JWT token when session created', async ({
+  assert,
+  client
+}) => {
+  const sessionPayload = {
+    email: 'guedes@acme.com',
+    password: '123456'
+  };
+
+  await Factory.model('App/Models/User').create(sessionPayload);
+
+  const response = await client
+    .post('/sessions')
+    .send(sessionPayload)
+    .end();
+
+  response.assertStatus(200);
+  assert.exists(response.body.token);
+});
