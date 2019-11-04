bf2276610f18acfd932f43a1e3545cf8e2d5c150
Fernando de Bona <fernando.reis@unimedcascavel.coop.br>
2019-11-04 15:15:25 -0300

    created user seed

--- backend/config/app.js
+++ backend/config/app.js
@@ -1,9 +1,7 @@
-
 /** @type {import('@adonisjs/framework/src/Env')} */
 const Env = use('Env');
 
 module.exports = {
-
   /*
   |--------------------------------------------------------------------------
   | Application Name
@@ -15,7 +13,7 @@ module.exports = {
   |
   */
 
-  name: Env.get('APP_NAME', 'AdonisJs'),
+  name: Env.get('APP_NAME', 'Sisteminha'),
 
   /*
   |--------------------------------------------------------------------------
@@ -80,7 +78,6 @@ module.exports = {
     |
     */
     jsonpCallback: 'callback',
-
 
     /*
     |--------------------------------------------------------------------------
---
+++ backend/database/seeds/UserSeeder.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/*
+|--------------------------------------------------------------------------
+| UserSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use('Factory');
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const User = use('App/Models/User');
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Role = use('App/Models/Role');
+
+const Database = use('Database');
+
+async function truncateTables() {
+  await User.truncate();
+  await Role.truncate();
+  await Database.table('user_roles').truncate();
+}
+
+class UserSeeder {
+  async run() {
+    await truncateTables();
+
+    const role = await Factory.model('App/Models/Role').make({
+      name: 'admin',
+    });
+
+    const userAdmin = await Factory.model('App/Models/User').create({
+      username: 'fbonareis',
+      email: 'fbonareis@gmail.com',
+      password: '123456',
+    });
+
+    await userAdmin.roles().save(role);
+
+    await Factory.model('App/Models/User').createMany(5);
+  }
+}
+
+module.exports = UserSeeder;
